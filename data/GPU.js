b5541966187bd6b6f4ed3fb30f2a9470175bcb80
yukai <1079869931@qq.com>
2021-08-12 15:36:10 +0800

    update

--- data/GPU.js
+++ data/GPU.js
@@ -1,10 +1,84 @@
 module.exports = {
   "gpuList": [{
-    "name": "3060",
+    "name": "RTX 3060",
     "value": "170"
   },
   {
-    "name": "3060ti",
+    "name": "RTX 3060 Ti",
     "value": "200"
+  },
+  {
+    "name": "RTX 3070",
+    "value": "220"
+  },
+  {
+    "name": "RTX 3070 Ti",
+    "value": "290"
+  },
+  ,
+  {
+    "name": "RTX 3080",
+    "value": "320"
+  },
+  {
+    "name": "RTX 3090",
+    "value": "350"
+  },
+  {
+    "name": "RTX 2060",
+    "value": "160"
+  },
+  {
+    "name": "RTX 2060 S",
+    "value": "175"
+  },
+  {
+    "name": "RTX 2070 S",
+    "value": "215"
+  },
+  {
+    "name": "RTX 2070",
+    "value": "175"
+  },
+  {
+    "name": "RTX 2080",
+    "value": "225"
+  },
+  {
+    "name": "RTX 2080 S",
+    "value": "250"
+  },
+  {
+    "name": "RTX 2080 Ti",
+    "value": "285"
+  },
+  {
+    "name": "GTX 1660 Ti",
+    "value": "120"
+  },
+  {
+    "name": "GTX 1660 S",
+    "value": "125"
+  },
+  {
+    "name": "GTX 1650",
+    "value": "75"
+  },
+  {
+    "name": "GTX 1050",
+    "value": "75"
+  },
+  {
+    "name": "GTX 1050 Ti",
+    "value": "75"
+  },{
+    "name": "GTX 1060",
+    "value": "120"
+  },{
+    "name": "GTX 1070",
+    "value": "150"
+  },{
+    "name": "GTX 1070 Ti",
+    "value": "180"
   }]
 }
